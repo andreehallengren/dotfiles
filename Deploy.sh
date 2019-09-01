92b761683e3c3d22e7cd5fac6e1fea73e38af1aa
Andree Hallengren <andree.hallengren@embark-studios.com>
2019-09-01 20:57:09 +0200

    cleanup

--- Deploy.sh
+++ Deploy.sh
@@ -2,19 +2,30 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 function cargoinstall() {
-    cargo install $1
+    if [ "type $1" ] 2>/dev/null; then
+        echo "-- $2 already installed, skipping.."
+        return
+    fi
+    cargo install $2
 }
 
 function snapinstall() {
-    if ! snap list | grep -q $1; then
-        snap install $1 --classic
+    if [ "type $1" ] 2>/dev/null; then
+        echo "-- $1 already installed, skipping.."
+        return
     fi
+
+    snap install $1 --classic
 }
 
 function aptinstall() {
+    if [ "hash $1" ] 2>/dev/null; then
+        echo "-- $1 already installed, skipping.."
+        return
+    fi
+
     # Second parameter is optional for custom repos
-    if [ $# -gt 1 ]
-    then
+    if [ $# -gt 1 ]; then
         sudo add-apt-repository $2
     fi
 
@@ -24,11 +35,10 @@ function aptinstall() {
 function link() {
     if [ -f "$HOME/$1" ]; then
         ln -sf $SCRIPT_DIR/$1 $HOME/$1
-        echo "symlinked: $SCRIPT_DIR/$1 -> $HOME/$1"
+        echo "-- symlinked: $SCRIPT_DIR/$1 -> $HOME/$1"
     else
-        echo "$HOME/$1 doesn't exists!"
+        echo "-- $HOME/$1 doesn't exists!"
     fi
-
 }
 
 function linkdir() {
@@ -43,33 +53,49 @@ function linkdir() {
     done
 }
 
+function createmissingfile() {
+    if ! [ "ls $1" &>/dev/null ]; then
+        touch $1
+    fi
+}
+
 # Install dependencies
-echo " === Installing dependencies === "
-sudo apt update
+echo "=== Installing dependencies === "
+[ -z "$(find -H /var/lib/apt/lists -maxdepth 0 -mtime -7)" ] && sudo apt update
 aptinstall cmake
 aptinstall curl
 aptinstall golang-go
 aptinstall zsh
 aptinstall alacritty ppa:mmstick76/alacritty
+
 # Install rust
-curl https://sh.rustup.rs -sSf | sh -s -- -y
-cargoinstall ripgrep
-cargoinstall exa
-cargoinstall bat
+if ! [ "type rustup" ] 2>/dev/null; then
+    curl https://sh.rustup.rs -sSf | sh -s -- -y
+else
+    echo "-- Rust already installed, skipping.."
+fi
+
+cargoinstall rg ripgrep
+cargoinstall exa exa
+cargoinstall bat bat
 
 snapinstall code
 
+if ! [ -d "$ZSH" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
+else
+    echo "-- Oh My Zsh already installed, skipping.."
+fi
+
 # Setup symbolic links
-echo " === Symlinking configurations === "
+echo "=== Symlinking configurations === "
 link .bashrc
 link .zshrc
-cp .tmux.conf $HOME/.tmux.conf
-cp .tmux.conf.local $HOME/.tmux.conf.local
+createmissingfile $HOME/.tmux.conf
+createmissingfile $HOME/.tmux.conf.local 
 link .tmux.conf
 link .tmux.conf.local
 linkdir .config
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 
 source $HOME/.bashrc
 source $HOME/.zshrc
